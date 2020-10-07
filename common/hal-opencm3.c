e78183b87352aada75332b36b49b8041a4aee1f1
Daan Sprenkels <daan@dsprenkels.com>
2020-10-07 10:50:04 +0200

    lm3s: Exit qemu when done

--- common/hal-opencm3.c
+++ common/hal-opencm3.c
@@ -49,6 +49,35 @@ extern uint32_t rcc_apb1_frequency;
 extern uint32_t rcc_apb2_frequency;
 #endif
 
+#if defined(LM3S)
+// TODO(dsprenkels) Currently, we only exit the QEMU host when a the program
+// exists sucessfully.  We should also populate some interrupts handlers that
+// occur on errors and/or other exception.
+
+// These two syscall values are used at the end of the program, when we want
+// to tell the QEMU host that we are done.  I took them from
+// <https://github.com/rust-embedded/cortex-m-semihosting/blob/8ab74cdb8c9ab669ded328072447ea6f6054ffe6/src/debug.rs#L25-L50>.
+static const uint32_t REPORT_EXCEPTION = 0x18;
+static const uint32_t ApplicationExit = 0x20026;
+
+// Do a system call towards QEMU or the debugger.
+static uint32_t semihosting_syscall(uint32_t nr, const uint32_t arg) {
+	__asm__ volatile (
+		"mov r0, %[nr]\n"
+		"mov r1, %[arg]\n"
+		"bkpt 0xAB\n"
+		"mov %[nr], r0\n"
+	: [nr] "+r" (nr) : [arg] "r" (arg) : "0", "1");
+	return nr;
+}
+
+// Register a destructor that will call qemu telling them that the program
+// has exited successfully.
+static void __attribute__ ((destructor)) semihosting_exit(void) {
+	semihosting_syscall(REPORT_EXCEPTION, ApplicationExit);
+}
+#endif /* defined(LM3S) */
+
 static void clock_setup(enum clock_mode clock)
 {
 #if defined(LM3S)
